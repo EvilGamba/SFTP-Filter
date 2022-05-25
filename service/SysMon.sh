684c6d858cfbde3931e84b56d9df2b6e9b0becf7
EvilGamba <58662253+EvilGamba@users.noreply.github.com>
2022-05-25 14:59:01 +0000

    Update SysMon.sh

--- service/SysMon.sh
+++ service/SysMon.sh
@@ -6,6 +6,6 @@ TARGET_PATH="/home"
 while true; do
     inotifywait -m -e close_write,moved_to -r $TARGET_PATH |
     while read dir action file; do
-    python3 /bin/upload_filter/filter.py ${TARGET_PATH}/${file}
+    python3 /bin/upload_filter/filter.py ${dir}/${file}
     done
 done
