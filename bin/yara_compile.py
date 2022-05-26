a2976af8e09955434c42645a88192c8ac02220fa
EvilGamba <58662253+EvilGamba@users.noreply.github.com>
2022-05-26 04:31:23 +0000

    Update yara_compile.py

--- bin/yara_compile.py
+++ bin/yara_compile.py
@@ -5,9 +5,11 @@ import yara
 def main():
     dic = {}
     i = 0
+    # set up working dir
     dir = r"/opt/SFTP-Filter/YARA_Rules/"
     for path in os.listdir(dir):
         if path.endswith((".yara", ".yar")):
+            #for every yara rules file and to a dictionary (you have to unfortunatley) and use the dictionary to compile all files together
            dic["ruleset {}".format(i)] = dir+path
            i += 1
     rules = yara.compile(filepaths = dic)
