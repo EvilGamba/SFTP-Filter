20445be5d52ae6ae7571b4453d5f4e795e5a636e
EvilGamba <58662253+EvilGamba@users.noreply.github.com>
2022-06-17 11:18:50 +0000

    Update filter.py

--- bin/filter.py
+++ bin/filter.py
@@ -7,30 +7,31 @@ def main():
     log = open(r"/etc/SFTP-Filter/history.log", 'a')
     filename = sys.argv[2]
     dir = sys.argv[1]
+    path = dir + filename
     
-    if not os.path.isfile(dir+filename):
-        log.write("ERROR:  {0} is not a file Or Does not Exist\n".format(filename))
+    if not os.path.isfile(path):
+        log.write("ERROR:  {0} is not a file Or Does not Exist\n".format(path))
         sys.exit("File Does Not Exist")
     
     namer = namelib()
     if not (namer.scan(filename)):
-        log.write("INFO: {0} : Did Not Pass Name Rules Check\n".format(filename))
-        os.remove(filename)
-        log.write("INFO: {0} : Deleted from Server\n".format(filename))
+        log.write("INFO: {0} : Did Not Pass Name Rules Check\n".format(path))
+        os.remove(path)
+        log.write("INFO: {0} : Deleted from Server\n".format(path))
         sys.exit(0)
-    log.write("INFO: {0} : Passed Name Check\n".format(filename))
+    log.write("INFO: {0} : Passed Name Check\n".format(path))
     
     sentry = guard()
-    if not (sentry.scan(dir+filename)):
-        log.write("INFO: {0} : Did Not Pass Security Check\n".format(filename))
-        os.remove(filename)
-        log.write("INFO: {0} : Deleted from Server\n".format(filename))
+    if not (sentry.scan(path)):
+        log.write("INFO: {0} : Did Not Pass Security Check\n".format(path))
+        os.remove(path)
+        log.write("INFO: {0} : Deleted from Server\n".format(path))
         sys.exit(0)
-    log.write("INFO: {0} : Passed Security Check\n".format(filename))
+    log.write("INFO: {0} : Passed Security Check\n".format(path))
 
     tinkerer = scripter()
-    log.write("INFO: {0} : Goes Through Scripts\n".format(filename))
-    tinkerer.run_scripts(dir+filename)
+    log.write("INFO: {0} : Goes Through Scripts\n".format(path))
+    tinkerer.run_scripts(path)
 
 
 
